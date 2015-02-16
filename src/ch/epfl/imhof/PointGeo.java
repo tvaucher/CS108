bd17e035c91bae823625882b02fcac4a83630494
Maxime Kjaer <maxime.kjaer@gmail.com>
2015-02-16 14:29:55 +0100

    Constructors & Getters in Point & PointGeo

--- src/ch/epfl/imhof/PointGeo.java
+++ src/ch/epfl/imhof/PointGeo.java
@@ -1,5 +1,24 @@
 package ch.epfl.imhof;
 
 public final class PointGeo {
-
+    private final double longitude;
+    private final double latitude;
+    public PointGeo (double longitude, double latitude) {
+        if (Math.abs(latitude) > Math.PI/2) {
+            throw new IllegalArgumentException("Latitude hors intervalle");
+        }
+        if (Math.abs(longitude) > Math.PI) {
+            throw new IllegalArgumentException("Longitude hors intervalle");
+        }
+        this.longitude = longitude;
+        this.latitude = latitude;
+    }
+    
+    public double latitude() {
+        return latitude;
+    }
+    
+    public double longitude() {
+        return longitude;
+    }
 }
--- src/ch/epfl/imhof/geometry/Point.java
+++ src/ch/epfl/imhof/geometry/Point.java
@@ -1,5 +1,17 @@
 package ch.epfl.imhof.geometry;
 
 public final class Point {
-
+    private final double x, y;
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+    
+    public double x() {
+        return x;
+    }
+    
+    public double y() {
+        return y;
+    }
 }
