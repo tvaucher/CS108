d21b5246ae8a3c82de3d25efacb6e99dc94f2475
Maxime Kjaer <maxime.kjaer@gmail.com>
2015-02-16 15:02:12 +0100

    Added Javadoc to Point & PointGeo

--- src/ch/epfl/imhof/PointGeo.java
+++ src/ch/epfl/imhof/PointGeo.java
@@ -1,23 +1,52 @@
 package ch.epfl.imhof;
 
+/**
+ * Un point à la surface de la Terre, en coordonnées sphériques.
+ * 
+ * @author Maxime Kjaer (250694)
+ */
 public final class PointGeo {
     private final double longitude;
     private final double latitude;
-    public PointGeo (double longitude, double latitude) {
+    /**
+     * Construit un point avec la latitude et la longitude en données.
+     * 
+     * @param longitude
+     *          La longitude du point, en radians.
+     * @param latitude
+     *          La latitude du point, en radians.
+     * @throws IllegalArgumentException
+     *          Si la longitude est invalide, c'est-à-dire hors de l'intervalle [-π; π]
+     * @throws IllegalArgumentException
+     *          Si la latitude est invalide, c'est-à-dire hors de l'intervalle [-π/2; π/2]
+     */
+    public PointGeo (double longitude, double latitude) throws IllegalArgumentException {
         if (Math.abs(latitude) > Math.PI/2) {
-            throw new IllegalArgumentException("Latitude hors intervalle");
+            throw new IllegalArgumentException("Latitude out of allowed range");
         }
         if (Math.abs(longitude) > Math.PI) {
-            throw new IllegalArgumentException("Longitude hors intervalle");
+            throw new IllegalArgumentException("Longitude out of allowed range");
         }
         this.longitude = longitude;
         this.latitude = latitude;
     }
     
+    /**
+     * Retourne la latitude du point, en radians.
+     * 
+     * @return latitude
+     *          La latitude du point, en radians.
+     */
     public double latitude() {
         return latitude;
     }
     
+    /**
+     * Retourne la longitude du point, en radians.
+     * 
+     * @return longitude
+     *          La longitude du point, en radians.
+     */
     public double longitude() {
         return longitude;
     }
--- src/ch/epfl/imhof/geometry/Point.java
+++ src/ch/epfl/imhof/geometry/Point.java
@@ -1,16 +1,41 @@
 package ch.epfl.imhof.geometry;
 
+/**
+ * Un point à la surface de la Terre, en coordonnées cartésiennes.
+ * 
+ * @author Maxime Kjaer (250694)
+ */
 public final class Point {
     private final double x, y;
+    /**
+     * Construit un point avec l'abscisse et l'ordonnée en données.
+     * 
+     * @param x
+     *          L'abscisse du point.
+     * @param y
+     *          L'ordonnée du point.
+     */
     public Point(double x, double y) {
         this.x = x;
         this.y = y;
     }
     
+    /**
+     * Retourne l'abscisse du point.
+     * 
+     * @return x
+     *          L'abscisse du point
+     */
     public double x() {
         return x;
     }
     
+    /**
+     * Retourne l'ordonnée du point.
+     * 
+     * @return y
+     *          L'ordonnée du point
+     */
     public double y() {
         return y;
     }
