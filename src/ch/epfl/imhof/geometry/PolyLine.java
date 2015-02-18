bbb2af91d7d22274f9e593b2d963cacb98ca1b2c
Maxime Kjaer <maxime.kjaer@gmail.com>
2015-02-18 14:14:37 +0100

    PolyLine class
    No documentation yet.

--- src/ch/epfl/imhof/geometry/PolyLine.java
+++ src/ch/epfl/imhof/geometry/PolyLine.java
@@ -1,8 +1,29 @@
 package ch.epfl.imhof.geometry;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public abstract class PolyLine {
+    private final List<Point> points;
     
+    public PolyLine(List<Point> points) throws IllegalArgumentException {
+        if (points.isEmpty()) {
+            throw new IllegalArgumentException("The list of points is empty");
+        }
+        ArrayList<Point> pointsCopy = new ArrayList<Point>(points);
+        this.points = Collections.unmodifiableList(pointsCopy);
+    }
     
+    public abstract boolean isClosed();
+    
+    public List<Point> points() {
+        return points;
+    }
+    
+    public Point firstPoint() {
+        return points.get(0);
+    }
     public static final class Builder {
         
     }
