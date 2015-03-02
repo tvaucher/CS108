9bb886deff50f2b772486b33b2387fb77fee70fb
Maxime Kjaer <maxime.kjaer@gmail.com>
2015-03-02 15:35:21 +0100

    Formatting of OSMWay

--- src/ch/epfl/imhof/osm/OSMWay.java
+++ src/ch/epfl/imhof/osm/OSMWay.java
@@ -8,60 +8,68 @@ import ch.epfl.imhof.Attributes;
 
 public final class OSMWay extends OSMEntity {
     private final List<OSMNode> nodes;
-    public OSMWay(long id, List<OSMNode> nodes, Attributes attributes) throws IllegalArgumentException {
+
+    public OSMWay(long id, List<OSMNode> nodes, Attributes attributes)
+            throws IllegalArgumentException {
         super(id, attributes);
-        if (nodes.size() < 2) throw new IllegalArgumentException("List of node must contain at least 2 elements");
-        this.nodes = Collections.unmodifiableList(new ArrayList<OSMNode>(nodes));
+        if (nodes.size() < 2)
+            throw new IllegalArgumentException(
+                    "List of node must contain at least 2 elements");
+        this.nodes = Collections
+                .unmodifiableList(new ArrayList<OSMNode>(nodes));
     }
 
     public int nodesCount() {
         return nodes.size();
     }
-    
+
     public List<OSMNode> nodes() {
         return nodes;
     }
-    
+
     public List<OSMNode> nonRepeatingNodes() {
         if (isClosed()) {
-            return Collections.unmodifiableList(new ArrayList<>(nodes.subList(0, nodes.size()-1)));
+            return Collections.unmodifiableList(new ArrayList<>(nodes.subList(
+                    0, nodes.size() - 1)));
             // TODO JUnit absolutely on this one
         }
         return nodes;
     }
-    
+
     public OSMNode firstNode() {
         return nodes.get(0);
     }
-    
+
     public OSMNode lastNode() {
-        return nodes.get(nodes.size()-1);
+        return nodes.get(nodes.size() - 1);
     }
-    
+
     public boolean isClosed() {
         return firstNode().equals(lastNode());
     }
-    
+
     public static final class Builder extends OSMEntity.Builder {
         private List<OSMNode> nodes;
-        
+
         public Builder(long id) {
             super(id);
             nodes = new ArrayList<>();
         }
-        
+
         public void addNode(OSMNode newNode) {
             nodes.add(newNode);
         }
-        
+
         public OSMWay build() throws IllegalStateException {
-            if (isIncomplete()) throw new IllegalStateException("Cannot build Incomplete Way");
-            return new OSMWay(id, nodes, b.build()); 
+            if (isIncomplete())
+                throw new IllegalStateException("Cannot build Incomplete Way");
+            return new OSMWay(id, nodes, b.build());
         }
-        
+
         @Override
         public boolean isIncomplete() {
-            if (nodes.size() < 2) return true;
+            if (nodes.size() < 2)
+                return true;
             return super.isIncomplete();
         }
     }
