58c20ae462c23b9e08b694d1ef67a0d624db76f6
Maxime Kjaer <maxime.kjaer@gmail.com>
2015-03-02 13:11:50 +0100

    Style changes to Polygon
    :ant: :panda_face:

--- src/ch/epfl/imhof/geometry/Polygon.java
+++ src/ch/epfl/imhof/geometry/Polygon.java
@@ -3,6 +3,7 @@ package ch.epfl.imhof.geometry;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import static java.util.Objects.requireNonNull;
 
 /**
  * A Polygon is a closed polyline that may or may not contain holes.
@@ -12,55 +13,56 @@ import java.util.List;
  */
 public final class Polygon {
     private final ClosedPolyLine shell;
-    private final List<ClosedPolyLine> holes; 
-    
+    private final List<ClosedPolyLine> holes;
+
     /**
-     * Construct a polygon with holes from a closed polyline (the shell)
-     * and a list of closed polylines (the holes). Ideally, we'd check
-     * that the holes indeed are inside the shell, but this is complicated
-     * and not strictly necessary for this project (we're going to assume
-     * that the data from OpenStreetMaps is correct).
+     * Construct a polygon with holes from a closed polyline (the shell) and a
+     * list of closed polylines (the holes). Ideally, we'd check that the holes
+     * indeed are inside the shell, but this is complicated and not strictly
+     * necessary for this project (we're going to assume that the data from
+     * OpenStreetMaps is correct).
      * 
      * @param shell
-     *          A closed polyline that marks the exterior of the polygon.
+     *            A closed polyline that marks the exterior of the polygon.
      * @param holes
-     *          A list of closed polylines contained in the holes
-     *          (but it won't be verified -- see above).
+     *            A list of closed polylines contained in the holes (but it
+     *            won't be verified -- see above).
      */
     public Polygon(ClosedPolyLine shell, List<ClosedPolyLine> holes) {
-        this.shell = shell; // The shell is already immutable, so there's no need for deep copies.
-        ArrayList<ClosedPolyLine> holesCopy = new ArrayList<ClosedPolyLine>(holes);
-        this.holes = Collections.unmodifiableList(holesCopy);
+        this.shell = shell; // The shell is already immutable, so there's no
+                            // need for deep copies.
+        this.holes = Collections
+                .unmodifiableList(new ArrayList<ClosedPolyLine>(
+                        requireNonNull(holes)));
     }
-    
+
     /**
      * Construct a polygon without holes from a closed polyline (the shell).
      * 
      * @param shell
-     *          A closed polyline that marks the exterior of the polygon.
+     *            A closed polyline that marks the exterior of the polygon.
      */
     public Polygon(ClosedPolyLine shell) {
-        this(shell, Collections.emptyList()); //invoke the other constructor with an explicit empty List
+        this(shell, Collections.emptyList()); // invoke the other constructor
+                                              // with an explicit empty List
     }
-    
+
     /**
      * Return the shell of the polygon.
      * 
-     * @return shell
-     *          The shell, the exterior of the polygon
+     * @return shell The shell, the exterior of the polygon
      */
     public ClosedPolyLine shell() {
         return shell;
     }
-    
+
     /**
      * Return a list of the holes of the polygon
      * 
-     * @return holes
-     *          A list of holes in the polygon.
+     * @return holes A list of holes in the polygon.
      */
     public List<ClosedPolyLine> holes() {
-        return holes; // holes is unmodifiable, so it's OK to return it like this.
+        return holes; // holes is unmodifiable, so it's OK to return it like
+                      // this.
     }
 }
-
