eeee4ce9aec92cdf9978c4765f612bb92ac9e8d3
Maxime Kjaer <maxime.kjaer@gmail.com>
2015-03-10 23:12:36 +0100

    Added preliminary OSMMapReader tests
    Everything seems to be working :satisfied:

---
+++ test/ch/epfl/imhof/osm/OSMMapReaderTest.java
@@ -0,0 +1,46 @@
+package ch.epfl.imhof.osm;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class OSMMapReaderTest {
+    private OSMMap bcMap = OSMMapReader.readOSMFile("bc.osm", false);
+    private OSMMap rlcMap = OSMMapReader.readOSMFile("lc.osm", false);
+    
+    // All expected values have been found by manually inspecting the OSM files
+    // (use Ctrl+F to get the number of ways / nodes / relations).
+    @Test
+    public void allWaysAreSaved() {
+        assertEquals(1, bcMap.ways().size());
+        assertEquals(49, rlcMap.ways().size());
+    }
+    
+    @Test
+    public void allRelationsAreSaved() {
+        assertEquals(0, bcMap.relations().size());
+        assertEquals(1, rlcMap.relations().size());
+    }
+    
+    @Test
+    public void tagsAreSavedCorrectly() {
+        // This can't really be much more than simple random sample testing.
+        // Nodes
+        // See lines 41-46 for the node (id 566975629)
+        // line 663 for the nd in the way (id 72320770)
+        OSMNode node = rlcMap.ways().get(20).nodes().get(3);
+        assertEquals("entrance", node.attributeValue("barrier"));
+        assertEquals("no", node.attributeValue("bicycle"));
+        assertEquals("entrance", node.attributeValue("building"));
+
+        // Ways
+        assertEquals("yes", bcMap.ways().get(0).attributeValue("building"));
+        assertEquals("BC", bcMap.ways().get(0).attributeValue("name"));
+        
+        // Relations
+        assertEquals("Rolex Learning Center", rlcMap.relations().get(0).attributeValue("name"));
+    }
+    
+    
+    
+}
