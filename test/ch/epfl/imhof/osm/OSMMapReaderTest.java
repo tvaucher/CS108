a4ba41d08fd7334008aedb9a04cb99f041c651e5
Erkanamen <timote.vaucher@epfl.ch>
2015-03-20 20:33:49 +0100

    Added some test files
    Also reads perfectly lc.osm => It's the only one I can check by eyes.
    Other are too large to do so. But still, looks great ! Less than 8 sec
    on my computer to read Lausanne + create associated map.

--- test/ch/epfl/imhof/osm/OSMMapReaderTest.java
+++ test/ch/epfl/imhof/osm/OSMMapReaderTest.java
@@ -1,18 +1,19 @@
 package ch.epfl.imhof.osm;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
+
+import ch.epfl.imhof.Map;
+import ch.epfl.imhof.projection.CH1903Projection;
 
 public class OSMMapReaderTest {
     
-    private OSMMap bcMap = OSMMapReader.readOSMFile("data/bc.osm", false);
-    private OSMMap rlcMap = OSMMapReader.readOSMFile("data/lc.osm", false);
-    
+    //private OSMMap bcMap = OSMMapReader.readOSMFile("data/bc.osm", false);
+    //private OSMMap rlcMap = OSMMapReader.readOSMFile("data/lc.osm", false);
+    //private OSMMap lausanneMap = OSMMapReader.readOSMFile("data/lausanne.osm.gz", true);
     
     // All expected values have been found by manually inspecting the OSM files
     // (use Ctrl+F to get the number of ways / nodes / relations).
-    @Test
+    /*@Test
     public void allWaysAreSaved() {        
         assertEquals(1, bcMap.ways().size());
         assertEquals(49, rlcMap.ways().size());
@@ -41,11 +42,15 @@ public class OSMMapReaderTest {
         
         // Relations
         assertEquals("Rolex Learning Center", rlcMap.relations().get(0).attributeValue("name"));
-    }
+    }*/
     
     @Test
     public void readTimer() {
-        OSMMapReader.readOSMFile("data/lausanne.osm.gz", true);
+        OSMToGeoTransformer transfo = new OSMToGeoTransformer(new CH1903Projection());
+        //transfo.transform(OSMMapReader.readOSMFile("data/lausanne.osm.gz", true));
+        String path = "data/lc.osm";//"data/sauvabelin.osm";//"data/lausanne.osm.gz";
+        OSMMap osmMap = OSMMapReader.readOSMFile(path, false);
+        Map map = transfo.transform(osmMap);
     }
     
     
