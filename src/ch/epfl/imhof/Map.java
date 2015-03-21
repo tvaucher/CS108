d1130b3ed265cad3ce737e73e77a37f38a00c0b7
Maxime Kjaer <maxime.kjaer@gmail.com>
2015-03-21 11:44:49 +0100

    Formatting and Javadoc in Map

--- src/ch/epfl/imhof/Map.java
+++ src/ch/epfl/imhof/Map.java
@@ -11,40 +11,94 @@ import static java.util.Objects.requireNonNull;
 import ch.epfl.imhof.geometry.PolyLine;
 import ch.epfl.imhof.geometry.Polygon;
 
+/**
+ * A map of geometrical objects (polylines, polygons and their attributes), as
+ * opposed to the rather abstract representation of objects that OSM uses.
+ * 
+ * @author Maxime Kjaer (250694)
+ * @author Timote Vaucher (246532)
+ * 
+ */
 public final class Map {
     private final List<Attributed<PolyLine>> polyLines;
     private final List<Attributed<Polygon>> polygons;
-    
-    public Map(List<Attributed<PolyLine>> polyLines, List<Attributed<Polygon>> polygons) {
-        this.polyLines = Collections.unmodifiableList(new ArrayList<>(requireNonNull(polyLines)));
-        this.polygons = Collections.unmodifiableList(new ArrayList<>(requireNonNull(polygons)));
+
+    /**
+     * Construct a new Map of polylines and polygons
+     * 
+     * @param polyLines
+     *            a list of attributed PolyLine objects
+     * @param polygons
+     *            a list of attributed Polygon objects
+     */
+    public Map(List<Attributed<PolyLine>> polyLines,
+            List<Attributed<Polygon>> polygons) {
+        this.polyLines = Collections.unmodifiableList(new ArrayList<>(
+                requireNonNull(polyLines)));
+        this.polygons = Collections.unmodifiableList(new ArrayList<>(
+                requireNonNull(polygons)));
     }
-    
+
+    /**
+     * Getter for the polylines of the map.
+     * 
+     * @return a list of all the attributed polylines in the map
+     */
     public List<Attributed<PolyLine>> polyLines() {
         return polyLines;
     }
-    
+
+    /**
+     * Getter for the polygons of the map
+     * 
+     * @return a list of all the attributed polygons in the map
+     */
     public List<Attributed<Polygon>> polygons() {
         return polygons;
     }
-    
+
+    /**
+     * A Builder for the Map, which will assist in the creation of a such object
+     * by allowing us to add polylines and polygons one at a time.
+     */
     public static class Builder {
         private List<Attributed<PolyLine>> polyLines = new ArrayList<>();
         private List<Attributed<Polygon>> polygons = new ArrayList<>();
-        
+
+        /**
+         * Add an attributed polyline to the list of polylines that will be a
+         * part of the map.
+         * 
+         * @param newPolyLine
+         *            a polyline with attributes.
+         */
         public void addPolyLine(Attributed<PolyLine> newPolyLine) {
             polyLines.add(newPolyLine);
         }
-        
+
+        /**
+         * Add an attributed polygon to the list of polyhons that will be a part
+         * of the map.
+         * 
+         * @param newPolygon
+         *            a polygon with attributes.
+         */
         public void addPolygon(Attributed<Polygon> newPolygon) {
             polygons.add(newPolygon);
         }
-        
+
+        /**
+         * Create a new Map object with the data that has been inputted into the
+         * Builder. Currently also prints a debugging file using the @see
+         * ch.epfl.imhof.Map#printMapInFile method.
+         * 
+         * @return the newly constructed Map
+         */
         public Map build() {
             printMapInFile();
             return new Map(polyLines, polygons);
         }
-        
+
         /**
          * A private method for debugging purposes that creates a file in
          * data/debug
@@ -53,8 +107,9 @@ public final class Map {
             File debugDirectory = new File("data/debug");
             if (debugDirectory.exists() && debugDirectory.isDirectory()) {
                 // Will normally be true only locally
-                try (PrintWriter pr = new PrintWriter(new File("data/debug/Map_"
-                        + System.currentTimeMillis() + ".txt"))) {
+                try (PrintWriter pr = new PrintWriter(
+                        new File("data/debug/Map_" + System.currentTimeMillis()
+                                + ".txt"))) {
                     long startTime = System.currentTimeMillis();
                     pr.println("START PRINTING MAP\n");
                     pr.println("THERE'S " + polyLines.size() + " POLYLINES");
@@ -62,10 +117,14 @@ public final class Map {
                     pr.println("START PRINTING POLYGONS");
                     int counter = 1;
                     for (Attributed<Polygon> polygon : polygons) {
-                        pr.println("  #"+ counter++ + " POLYGON");
-                        //pr.println("  AREA OF " + polygon.value().shell().area());
-                        pr.println("    HAS " + polygon.value().shell().points().size() + " POINTS");
-                        pr.println("    HAS " + polygon.value().holes().size() + " HOLES");
+                        pr.println("  #" + counter++ + " POLYGON");
+                        // pr.println("  AREA OF " +
+                        // polygon.value().shell().area());
+                        pr.println("    HAS "
+                                + polygon.value().shell().points().size()
+                                + " POINTS");
+                        pr.println("    HAS " + polygon.value().holes().size()
+                                + " HOLES");
                     }
                     pr.println("\nWRITTEN IN "
                             + (System.currentTimeMillis() - startTime) / 1000.
