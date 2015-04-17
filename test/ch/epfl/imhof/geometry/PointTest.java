aac31494fa5985ae48dafe2ef24ee4a12fad1450
Maxime Kjaer <maxime.kjaer@gmail.com>
2015-04-17 12:38:51 +0200

    Added alignedCoordinateChange
    It is now possible to change the frame of a vector space using
    Point.alignedCoordinateChange.
    
    It does not support rotation of the frame, though. Giving it "bad"
    values (i.e. two initial points with the same x or y, or just two
    identical points) will result in an IllegalArgumentException since no
    information can be gathered from such data.

--- test/ch/epfl/imhof/geometry/PointTest.java
+++ test/ch/epfl/imhof/geometry/PointTest.java
@@ -2,6 +2,8 @@ package ch.epfl.imhof.geometry;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.function.Function;
+
 import org.junit.Test;
 
 public class PointTest {
@@ -17,5 +19,18 @@ public class PointTest {
     public void yGetterReturnsY() {
         for (double y = -100; y <= 100; y += 12.32)
             assertEquals(y, new Point(0, y).y(), DELTA);
+    }
+    
+    @Test
+    public void alignedCoordinateChangeBasicTest() {
+        Function<Point, Point> blueToRed =
+                Point.alignedCoordinateChange(new Point(1, -1),
+                                              new Point(5, 4),
+                                              new Point(-1.5, 1),
+                                              new Point(0, 0));
+        Point newPoint = blueToRed.apply(new Point(0, 0)); // retourne le point (3,2)
+        System.out.println(newPoint.x());
+        assertEquals(3, newPoint.x(), DELTA);
+        assertEquals(2, newPoint.y(), DELTA);
     }
 }
