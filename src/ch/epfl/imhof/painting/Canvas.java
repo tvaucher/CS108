de1beee39b3c66b1614e3571bca0603cef388556
Maxime Kjaer <maxime.kjaer@gmail.com>
2015-04-18 01:43:14 +0200

    Added JavaDoc for the canvas
    +formatting.

--- src/ch/epfl/imhof/painting/Canvas.java
+++ src/ch/epfl/imhof/painting/Canvas.java
@@ -3,7 +3,33 @@ package ch.epfl.imhof.painting;
 import ch.epfl.imhof.geometry.PolyLine;
 import ch.epfl.imhof.geometry.Polygon;
 
+/**
+ * Canvas is a simple interface containing two abstract methods, since all
+ * canvases must be able to draw polygons and polylines as a part of this
+ * project.
+ * 
+ * @author Maxime Kjaer (250694)
+ * @author Timote Vaucher (246532)
+ */
 public interface Canvas {
+    /**
+     * A method that draws a PolyLine onto the canvas.
+     * 
+     * @param polyLine
+     *            The PolyLine that will be drawn onto the canvas.
+     * @param style
+     *            The LineStyle of said PolyLine.
+     */
     public void drawPolyLine(PolyLine polyLine, LineStyle style);
+
+    /**
+     * A method that draws a Polygon onto the canvas, filling it with the given
+     * Color.
+     * 
+     * @param polygon
+     *            The Polygon that will be drawn onto the canvas.
+     * @param color
+     *            The Color that will be used to fill said Polygon.
+     */
     public void drawPolygon(Polygon polygon, Color color);
 }
--- src/ch/epfl/imhof/painting/Java2DCanvas.java
+++ src/ch/epfl/imhof/painting/Java2DCanvas.java
@@ -16,92 +16,152 @@ import java.awt.image.BufferedImage;
 import static java.awt.RenderingHints.KEY_ANTIALIASING;
 import static java.awt.RenderingHints.VALUE_ANTIALIAS_ON;
 
+/**
+ * Java2DCanvas represents a concrete implementation of the canvas that we need
+ * in order to draw the map in this project.
+ * 
+ * @author Maxime Kjaer (250694)
+ * @author Timote Vaucher (246532)
+ */
 public class Java2DCanvas implements Canvas {
     private final BufferedImage image;
     private final Graphics2D ctx;
-    
+
     private final Function<Point, Point> projectedToCanvas;
     private final static int CANVAS_DPI = 72;
-    //private final double scalingFactor;
-    
-    public Java2DCanvas(Point bl, Point tr, int width, int height, int dpi, Color bg) {
+
+    // private final double scalingFactor;
+
+    /**
+     * Constructs a new Java2D object.
+     * 
+     * @param bl
+     *            The bottom-left Point of the Canvas.
+     * @param tr
+     *            The top-right Point of the Canvas (should be above and to the
+     *            right of bl)
+     * @param width
+     *            The width of the Canvas, in pixels (should be >=0).
+     * @param height
+     *            The height of the Canvas, in pixels (should be >=0).
+     * @param dpi
+     *            The resolution of the Canvas image, in dots per inch (should
+     *            be >=0).
+     * @param bg
+     *            The background Color of the Canvas.
+     * 
+     * @throws IllegalArgumentException
+     *             If any of the parameters are outside of their allowed ranges,
+     *             or are nonsensical (e.g. a bottom-left point that is above
+     *             the top-right)
+     */
+    public Java2DCanvas(Point bl, Point tr, int width, int height, int dpi,
+            Color bg) {
         if (height < 0 || width < 0)
-            throw new IllegalArgumentException("Width and height must be bigger than 0");
+            throw new IllegalArgumentException(
+                    "Width and height must be bigger than 0");
         if (dpi < 0)
-            throw new IllegalArgumentException("Resolution must be bigger than 0");
+            throw new IllegalArgumentException(
+                    "Resolution must be bigger than 0");
         if (bl.x() > tr.x() || bl.y() > tr.y())
-            throw new IllegalArgumentException("The bottom left point must be below and to the left of the top right point");
+            throw new IllegalArgumentException(
+                    "The bottom left point must be below and to the left of the top right point");
         image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
         ctx = image.createGraphics();
-        
-        //Set antialiasing
-        ctx.setRenderingHint(KEY_ANTIALIASING,
-                VALUE_ANTIALIAS_ON);
-        
+
+        // Set antialiasing
+        ctx.setRenderingHint(KEY_ANTIALIASING, VALUE_ANTIALIAS_ON);
+
         ctx.setColor(bg.toAWTColor());
         ctx.fillRect(0, 0, width, height);
-        //Change the scale
-        double scalingFactor = dpi*1d/CANVAS_DPI;
+        // Change the scale
+        double scalingFactor = dpi * 1d / CANVAS_DPI;
         ctx.scale(scalingFactor, scalingFactor);
-        projectedToCanvas = Point.alignedCoordinateChange(bl, new Point(0, height/scalingFactor), tr, new Point(width/scalingFactor, 0));
-        //Set Background color
-        //ctx.setBackground(bg.toAWTColor());
-        //System.out.println(ctx.getBackground().getRGB() + "RGB : " + ctx.getBackground().getRed() + " r " + ctx.getBackground().getGreen() + " g " + ctx.getBackground().getBlue() + " b");
+        projectedToCanvas = Point.alignedCoordinateChange(bl, new Point(0,
+                height / scalingFactor), tr,
+                new Point(width / scalingFactor, 0));
+        // Set Background color
+        // ctx.setBackground(bg.toAWTColor());
+        // System.out.println(ctx.getBackground().getRGB() + "RGB : " +
+        // ctx.getBackground().getRed() + " r " + ctx.getBackground().getGreen()
+        // + " g " + ctx.getBackground().getBlue() + " b");
     }
-    
-    public BufferedImage image() { return image; }
-    
+
+    /**
+     * A getter for the image on the canvas.
+     * 
+     * @return A BufferedImage object representing what has been drawn onto the
+     *         canvas.
+     */
+    public BufferedImage image() {
+        return image;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * ch.epfl.imhof.painting.Canvas#drawPolyLine(ch.epfl.imhof.geometry.PolyLine
+     * , ch.epfl.imhof.painting.LineStyle)
+     */
     @Override
     public void drawPolyLine(PolyLine polyLine, LineStyle style) {
         int cap = 0, join = 0;
-        
-        //Generates cap style for BasicStroke type
+
+        // Generates cap style for BasicStroke type
         switch (style.lineCap()) {
-        case BUTT :
+        case BUTT:
             cap = BasicStroke.CAP_BUTT;
             break;
-        case ROUND :
+        case ROUND:
             cap = BasicStroke.CAP_ROUND;
             break;
-        case SQUARE :
+        case SQUARE:
             cap = BasicStroke.CAP_SQUARE;
             break;
         }
-        
-        //Generates join style for BasicStroke type
+
+        // Generates join style for BasicStroke type
         switch (style.lineJoin()) {
-        case BEVEL :
+        case BEVEL:
             join = BasicStroke.JOIN_BEVEL;
             break;
-        case MITER :
+        case MITER:
             join = BasicStroke.JOIN_MITER;
             break;
-        case ROUND :
+        case ROUND:
             join = BasicStroke.JOIN_ROUND;
             break;
         }
-        
-        //parameters for the stroke
+
+        // parameters for the stroke
         ctx.setColor(style.color().toAWTColor());
-        BasicStroke stroke = style.dashingPattern().length == 0 ? new BasicStroke(style.width(),
-                cap, join, 10f) : new BasicStroke(style.width(),
-                        cap, join, 10f, style.dashingPattern(), 0f);
-        
-        //creating the polygon
+        BasicStroke stroke = style.dashingPattern().length == 0 ? new BasicStroke(
+                style.width(), cap, join, 10f) : new BasicStroke(style.width(),
+                cap, join, 10f, style.dashingPattern(), 0f);
+
+        // creating the polygon
         ctx.draw(stroke.createStrokedShape(pathPolyLine(polyLine)));
     }
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * ch.epfl.imhof.painting.Canvas#drawPolygon(ch.epfl.imhof.geometry.Polygon,
+     * ch.epfl.imhof.painting.Color)
+     */
     @Override
     public void drawPolygon(Polygon polygon, Color color) {
         ctx.setColor(color.toAWTColor());
-        //creating the shell
+        // creating the shell
         Area shell = new Area(pathPolyLine(polygon.shell()));
         for (PolyLine p : polygon.holes()) {
             shell.subtract(new Area(pathPolyLine(p)));
         }
         ctx.fill(shell);
     }
-    
+
     private Shape pathPolyLine(PolyLine polyLine) {
         boolean first = true;
         Path2D.Double path = new Path2D.Double();
