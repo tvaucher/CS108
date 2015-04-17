2c095bd250b8d4793005713ad3887f815f6d8e18
Maxime Kjaer <maxime.kjaer@gmail.com>
2015-04-17 12:40:32 +0200

    Woops! Added alignedCoordinateChange
    (not the test)
    
    See aac31494fa5985ae48dafe2ef24ee4a12fad1450

--- src/ch/epfl/imhof/geometry/Point.java
+++ src/ch/epfl/imhof/geometry/Point.java
@@ -1,5 +1,7 @@
 package ch.epfl.imhof.geometry;
 
+import java.util.function.Function;
+
 /**
  * A point on Earth's surface, in Cartesian coordinates.
  * 
@@ -38,5 +40,24 @@ public final class Point {
      */
     public double y() {
         return y;
+    }
+
+    public static Function<Point, Point> alignedCoordinateChange(
+            Point oldPoint1, Point newPoint1, Point oldPoint2, Point newPoint2) {
+        if (oldPoint1.x == oldPoint2.x || oldPoint1.y == oldPoint2.y)
+            throw new IllegalArgumentException(
+                    "Please give two different, valid points the initial coordinates (they should not have the same x or y)");
+
+        // We need to find two equations of the type (ax)*X+(bx) and
+        // (ay)*Y+(by), which will handle both translation and dilation.
+        double ax = (newPoint1.x - newPoint2.x) / (oldPoint1.x - oldPoint2.x);
+        double bx = newPoint2.x + Math.signum(oldPoint2.x) * ax
+                * (oldPoint2.x / oldPoint1.x);
+
+        double ay = (newPoint1.y - newPoint2.y) / (oldPoint1.y - oldPoint2.y);
+        double by = newPoint2.y + Math.signum(oldPoint2.y) * ay
+                * (oldPoint2.y / oldPoint1.y);
+
+        return point -> new Point(ax * point.x + bx, ay * point.y + by);
     }
 }
