8a7b4b36d5d654b0d4f90d2ac70af104af741b69
Maxime Kjaer <maxime.kjaer@gmail.com>
2015-04-24 18:27:47 +0200

    Correction 5: TINY optimization
    Though the compiler should catch this, we lose points if we don't have
    this minuscule optimization.
    
    This commit addresses the following issue:
    
    *** Méthode project [1/2]
    -1: Lorsque vous utilisez plusieurs fois le même résultat d'un calcul il
    est préférable de le stocker dans une variable.
    
    *** Méthode inverse [2/2]
    -0: Lorsque vous utilisez plusieurs fois le même résultat d'un calcul il
    est préférable de le stocker dans une variable.

--- src/ch/epfl/imhof/projection/CH1903Projection.java
+++ src/ch/epfl/imhof/projection/CH1903Projection.java
@@ -24,18 +24,21 @@ public final class CH1903Projection implements Projection {
     public Point project(PointGeo point) {
         double longitudeDegrees = Math.toDegrees(point.longitude());
         double latitudeDegrees = Math.toDegrees(point.latitude());
-        double longitude1 = (1. / 10000) * (longitudeDegrees * 3600 - 26782.5);
-        double latitude1 = (1. / 10000) * (latitudeDegrees * 3600 - 169028.66);
+        double oneTenthousands = (1. / 10000);
+        double longitude1 = oneTenthousands * (longitudeDegrees * 3600 - 26782.5);
+        double latitude1 = oneTenthousands * (latitudeDegrees * 3600 - 169028.66);
+        double long1TimesLat1 = longitude1 * latitude1;
+        double long1Squared = longitude1 * longitude1;
         double x = 600072.37
                 + 211455.93 * longitude1 
-                - 10938.51 * longitude1 * latitude1 
-                - .36 * longitude1 * latitude1 * latitude1 
+                - 10938.51 * long1TimesLat1
+                - .36 * long1TimesLat1 * latitude1 
                 - 44.54 * Math.pow(longitude1, 3);
         double y = 200147.07 
                 + 308807.95 * latitude1 
-                + 3745.25 * longitude1 * longitude1 
+                + 3745.25 * long1Squared 
                 + 76.63 * latitude1 * latitude1 
-                - 194.56 * longitude1 * longitude1 * latitude1 
+                - 194.56 * long1Squared * latitude1 
                 + 119.79 * Math.pow(latitude1, 3);
         return new Point(x, y);
     }
@@ -50,19 +53,22 @@ public final class CH1903Projection implements Projection {
     public PointGeo inverse(Point point) {
         double x1 = (point.x() - 6e5) / 1e6;
         double y1 = (point.y() - 2e5) / 1e6;
+        double x1TimesY1 = x1 * y1;
+        double x1Squared = x1 * x1;
         double longitude0 = 2.6779094 
-                + 4.728982 * x1 
-                + 0.791484 * x1 * y1
-                + 0.1306 * x1 * y1 * y1 
+                + 4.728982 * x1
+                + 0.791484 * x1TimesY1
+                + 0.1306 * x1TimesY1 * y1 
                 - 0.0436 * Math.pow(x1, 3);
         double latitude0 = 16.9023892 
                 + 3.238272 * y1 
-                - 0.270978 * x1 * x1
+                - 0.270978 * x1Squared
                 - 0.002528 * y1 * y1 
-                - 0.0447 * x1 * x1 * y1 
+                - 0.0447 * x1Squared * y1 
                 - 0.014 * Math.pow(y1, 3);
-        double longitudeRadians = Math.toRadians(longitude0 * 100 / 36);
-        double latitudeRadians = Math.toRadians(latitude0 * 100 / 36);
+        double conversionFraction = 100 / 36;
+        double longitudeRadians = Math.toRadians(longitude0 * conversionFraction);
+        double latitudeRadians = Math.toRadians(latitude0 * conversionFraction);
 
         return new PointGeo(longitudeRadians, latitudeRadians);
     }
