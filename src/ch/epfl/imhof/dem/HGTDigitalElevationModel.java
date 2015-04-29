700c775c447ef1e004b586bd0cd9d12a1b02fd55
Erkanamen <timote.vaucher@epfl.ch>
2015-04-29 14:37:38 +0200

    DEM p0

--- src/ch/epfl/imhof/dem/HGTDigitalElevationModel.java
+++ src/ch/epfl/imhof/dem/HGTDigitalElevationModel.java
@@ -1,27 +1,39 @@
 package ch.epfl.imhof.dem;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.ShortBuffer;
+import java.nio.channels.FileChannel.MapMode;
 
 import ch.epfl.imhof.PointGeo;
 import ch.epfl.imhof.Vector3;
 
 public class HGTDigitalElevationModel implements DigitalElevationModel {
+    //private final PointGeo bl;
+    private final FileInputStream stream;
+    private final ShortBuffer buffer;
     
-    public HGTDigitalElevationModel(File hgt) {
-        // TODO Auto-generated constructor stub
+    public HGTDigitalElevationModel(File hgt) throws IOException {
+        
+        long length = 100; //useless value ATM
+        stream = new FileInputStream(hgt);
+        buffer = stream.getChannel()
+                .map(MapMode.READ_ONLY, 0, length)
+                .asShortBuffer();
     }
     
     
     @Override
     public void close() throws Exception {
-        // TODO Auto-generated method stub
-
+        stream.close();
     }
 
     @Override
     public Vector3 normalAt(PointGeo p) throws IllegalArgumentException {
         // TODO Auto-generated method stub
+        
         return null;
     }
-
+    
 }
