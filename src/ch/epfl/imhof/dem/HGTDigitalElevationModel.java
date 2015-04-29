4e4223dc6ffa184480b96ef619ecf7f4be2c80c5
Maxime Kjaer <maxime.kjaer@gmail.com>
2015-04-29 23:17:56 +0200

    Added the normalAt(PointGeo p) method
    We can still review and test this.

--- src/ch/epfl/imhof/dem/HGTDigitalElevationModel.java
+++ src/ch/epfl/imhof/dem/HGTDigitalElevationModel.java
@@ -12,10 +12,14 @@ import ch.epfl.imhof.PointGeo;
 import ch.epfl.imhof.Vector3;
 
 public class HGTDigitalElevationModel implements DigitalElevationModel {
+    private static final double DELTA = 0.000001;
     private final PointGeo bl, tr;
-    private final double sigma; // [rad]
+    private final double delta; // [rad]
+    private final double s; // the distance a dot and the next, in m
     private final FileInputStream stream;
     private final ShortBuffer buffer;
+    private final int side; // The number of points on one axis in the
+                            // altitude plot (usually 3601)
 
     private final static Pattern hgtPattern = Pattern
             .compile("^([NS])(\\d{2})([EW])(\\d{3}).hgt$");
@@ -26,13 +30,15 @@ public class HGTDigitalElevationModel implements DigitalElevationModel {
         Matcher m = hgtPattern.matcher(hgt.getName());
         if (!m.matches())
             throw new IllegalArgumentException("Invalid file name");
-        
-        //Test about the length
+
+        // Test about the length
         long length = hgt.length();
-        double side = Math.sqrt(length);
-        if (side % 2 != 0)
+        double dSide = Math.sqrt(length / 2);
+        if (dSide % 1 >= DELTA)
             throw new IllegalArgumentException(
                     "File must contain a sqrt of length must be even and an integer");
+
+        side = (int) dSide;
 
         // Creation of bottom left point
         double lat = Math.toRadians((m.group(1).equals("N") ? 1d : -1d)
@@ -43,7 +49,8 @@ public class HGTDigitalElevationModel implements DigitalElevationModel {
         tr = new PointGeo(lon + oneDegToRad, lat + oneDegToRad);
 
         // Computation of resolution
-        sigma = oneDegToRad / (side / 2);
+        delta = oneDegToRad / side;
+        s = Earth.RADIUS * delta;
         // 1 deg (in rad) contains side points (2 bytes/point)
 
         // Creation of buffer for reading
@@ -60,15 +67,48 @@ public class HGTDigitalElevationModel implements DigitalElevationModel {
     @Override
     public Vector3 normalAt(PointGeo p) throws IllegalArgumentException {
         if (!contains(p))
-            throw new IllegalArgumentException("Your point is in another file"); //Mario ref
-        //notes : sigma is already in radians
-        //You can use oneDegToRad if you want a const that transforms 1 deg in rad.
-        //there already is bl and tr points, if you want any other feel free to had.
-        
-        return null;
+            throw new IllegalArgumentException("Your point is in another file"); // Mario
+                                                                                 // ref
+        // AKA Your princess is in another castle
+
+        // Calculating the grid number that we have to get data from.
+        int i = (int) ((p.longitude() - bl.longitude()) / delta);
+        int j = (int) ((tr.latitude() - p.latitude()) / delta);
+
+        // z will be our table of heights for the 4 points around p.
+        short[][] z = new short[2][2];
+        // Fetching heights of the 4 points around p
+        z[0][0] = buffer.get(i + side * j);
+        z[0][1] = buffer.get(i + side * j + 1);
+        z[1][0] = buffer.get(i + side * j + side);
+        z[1][1] = buffer.get(i + side * j + 1 + side);
+
+        // For the sake of legibility:
+        i = 0;
+        j = 0;
+        double halfS = 0.5 * s; // Cache it this time around (whether this
+                                // optimizes the performance is dubious at best,
+                                // but points were taken off last time).
+        double n1 = halfS
+                * (z[i][j] - z[i + 1][j] + z[i][j + 1] - z[i + 1][j + 1]);
+        double n2 = halfS
+                * (z[i][j] + z[i + 1][j] - z[i][j + 1] - z[i + 1][j + 1]);
+        double n3 = s * s;
+
+        /*
+         * For debugging purposes, we can print the points around p:
+         * System.out.println("(i, j): " + z[0][0]);
+         * System.out.println("(i, j+1): " + z[0][1]);
+         * System.out.println("(i+1, j): " + z[1][0]);
+         * System.out.println("(i+1, j+1): " + z[1][1]);
+         */
+
+        return new Vector3(n1, n2, n3);
     }
 
     private boolean contains(PointGeo p) {
+        // TODO Would this work south of the Equator? Should we put Math.abs on
+        // everything?
         return p.latitude() >= bl.latitude() && p.longitude() >= bl.longitude()
                 && p.latitude() <= tr.latitude()
                 && p.longitude() <= tr.longitude();
