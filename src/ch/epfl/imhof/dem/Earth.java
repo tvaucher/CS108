e07f45accea2bdb4a1518da60197eec5f24d3740
Maxime Kjaer <maxime.kjaer@gmail.com>
2015-04-29 14:01:48 +0200

    Added a Digital Elevation Model package
    + a tiny container for Earth's constants

---
+++ src/ch/epfl/imhof/dem/Earth.java
@@ -0,0 +1,12 @@
+package ch.epfl.imhof.dem;
+
+/**
+ * The only goal of this interface is to hold some constants related to the
+ * planet Earth.
+ * 
+ * @author Maxime Kjaer (250694)
+ * @author Timote Vaucher (246532)
+ */
+public interface Earth {
+    public final static int RADIUS = 6_378_137;
+}
