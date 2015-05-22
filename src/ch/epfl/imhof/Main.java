8879818ae5d4da52a2bc79db77753208ef1cdfad
Maxime Kjaer <maxime.kjaer@gmail.com>
2015-05-23 00:00:48 +0200

    Removed unused imports in Main

--- src/ch/epfl/imhof/Main.java
+++ src/ch/epfl/imhof/Main.java
@@ -1,51 +1,9 @@
 package ch.epfl.imhof;
 
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.util.regex.Pattern;
-
-
-import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JSpinner;
-import javax.swing.JTabbedPane;
-import javax.swing.JTextField;
-import javax.swing.SpinnerNumberModel;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
-import javax.swing.border.EmptyBorder;
-import javax.swing.filechooser.FileFilter;
-import javax.swing.filechooser.FileNameExtensionFilter;
 
-
-import ch.epfl.imhof.dem.Earth;
-import ch.epfl.imhof.dem.HGTDigitalElevationModel;
-import ch.epfl.imhof.dem.ReliefShader;
-import ch.epfl.imhof.geometry.Point;
 import ch.epfl.imhof.gui.GraphicalUserInterface;
-import ch.epfl.imhof.gui.OpenFileButton;
-import ch.epfl.imhof.gui.SaveFileButton;
-import ch.epfl.imhof.osm.OSMMap;
-import ch.epfl.imhof.osm.OSMMapReader;
-import ch.epfl.imhof.osm.OSMToGeoTransformer;
-import ch.epfl.imhof.painting.Color;
-import ch.epfl.imhof.painting.Java2DCanvas;
-import ch.epfl.imhof.painting.Painter;
-import ch.epfl.imhof.painting.SwissPainter;
-import ch.epfl.imhof.projection.CH1903Projection;
-import ch.epfl.imhof.projection.Projection;
-import ch.epfl.imhof.utility.MapMaker;
-import ch.epfl.imhof.utility.QueryGenerator;
 
 /**
  * The Main class is what ties everything else together. It's the main
