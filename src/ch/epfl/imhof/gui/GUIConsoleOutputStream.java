06b60a5c064a5960ff0921f7e141c809a1e280a1
Maxime Kjaer <maxime.kjaer@gmail.com>
2015-05-25 16:29:45 +0200

    Added JavaDoc to GUIConsoleOutputStream

--- src/ch/epfl/imhof/gui/GUIConsoleOutputStream.java
+++ src/ch/epfl/imhof/gui/GUIConsoleOutputStream.java
@@ -5,13 +5,30 @@ import java.io.OutputStream;
 
 import javax.swing.JTextArea;
 
+/**
+ * The GUIConsoleOutputStream is an OutputStream that prints in a JTextArea in
+ * the GUI. In this projet, we need it in order to use System.out.println and
+ * have that output directly to the GUI's console.
+ * 
+ * @author Maxime Kjaer (250694)
+ * @author Timote Vaucher (246532)
+ */
 public final class GUIConsoleOutputStream extends OutputStream {
     private JTextArea textArea;
 
+    /**
+     * Constructs a new GUIConsoleOutputStream.
+     * 
+     * @param textArea
+     *            The JTextArea that the streams outputs to.
+     */
     public GUIConsoleOutputStream(JTextArea textArea) {
         this.textArea = textArea;
     }
 
+    /* (non-Javadoc)
+     * @see java.io.OutputStream#write(int)
+     */
     @Override
     public void write(int b) throws IOException {
         textArea.append(String.valueOf((char) b));
